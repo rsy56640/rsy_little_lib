fd63172dfc4e8b7abc05253217fcd2218959f1f6
rsy56640 <851911672@qq.com>
2018-04-05 20:09:00 +0800

    init

---
+++ library_for_algorithm/RB_Tree/RB_Tree_2/RB_Tree.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _RB_TREE_H
+#define _RB_TREE_H
+#include "RB_Tree_Iterator.h"
+
+namespace RSY_TOOL
+{
+
+
+	namespace MY_RB_Tree
+	{
+
+
+
+
+
+	}
+
+}
+#endif // !_RB_TREE_H
---
+++ library_for_algorithm/RB_Tree/RB_Tree_2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "RB_Tree.h"
+using namespace std;
+using namespace RSY_TOOL::MY_RB_Tree;
+
+int main()
+{
+
+
+
+	system("pause");
+	return 0;
+}

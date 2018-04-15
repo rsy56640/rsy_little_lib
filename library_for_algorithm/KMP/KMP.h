865f3083fe5d30317f6b1b460a84f98ca780278f
rsy56640 <851911672@qq.com>
2018-04-16 00:30:42 +0800

    update

--- library_for_algorithm/KMP/KMP.h
+++ library_for_algorithm/KMP/KMP.h
@@ -43,6 +43,11 @@ namespace RSY_TOOL
 
 
 		/*
+		 * Generics program for KMP searching algorithm.
+		 * Require random-access-iterator.
+		**/
+
+		/*
 		 * verify whether the iterator is random accessable.
 		**/
 		namespace
@@ -58,7 +63,6 @@ namespace RSY_TOOL
 
 		/*
 		 * KMP TEMPLATE
-		 *
 		**/
 		//@ Parameter list:
 		//@     _text_First:
@@ -85,19 +89,64 @@ namespace RSY_TOOL
 			const difference_type _pattern_length = _pattern_Last - _pattern_First;
 
 
-			//compute prefix function
+			/*
+			 * compute prefix function
+			**/
+			std::vector<difference_type> prefix(_pattern_length);
+			prefix[0] = -1;
+
+			//cur_pos must always be the backward last position of the prefix string 
+			//that the previous prefix[i-1] matches.
+			difference_type cur_pos = -1;
+
+			//compute every prefix[i]
+			for (difference_type i = 1; i < _pattern_length; ++i)
+			{
+
+				//0 1 2, ..., cur, cur+1, ..., i-1, i, ...
+				//A B C  ...   X     Y
+				//       A B C      ...         X   ?
+				while (cur_pos >= 0 && *(_pattern_First + (cur_pos + 1)) != *(_pattern_First + i))
+					cur_pos = prefix[cur_pos];
+
+				if (*(_pattern_First + (cur_pos + 1)) == *(_pattern_First + i))
+					cur_pos++;
+
+				prefix[i] = cur_pos;
+
+			}
 
 
-
-
-
-
-
-
+			/*
+			 * Knuth-Morris-Pratt algorithm
+			 * It's used to match the pattern in the text.
+			 * Time Complexity: O(n), n is the length of the text.
+			**/
 			std::vector<_InIt> result;
 
+			int q = 0;			//number of characters matched
+
+			for (auto it = _text_First; it != _text_Last; ++it)
+			{
+
+				while (q > 0 && *(_pattern_First + q) != *it)
+					q = prefix[q - 1] + 1;
+
+				if (*(_pattern_First + q) == *it)
+					q++;
+
+				if (q == _pattern_length)
+				{
+					result.push_back(it - (_pattern_length - 1));
+					q = prefix[q - 1] + 1;
+				}
+
+			}
+
+
 			return result;
-		}
+
+		}//end TEMPLATE METHOD KMP
 
 
 	}//end namespace KMP
--- library_for_algorithm/KMP/main.cpp
+++ library_for_algorithm/KMP/main.cpp
@@ -17,18 +17,25 @@ int main()
 
 	string pattern = "abxab";
 	string text = "abxcabxabaxabxababx";
-	int size = pattern.size();
 
-	vector<int> prefix(size);
-	kmp_prefix_function(pattern, prefix);
+
 	auto result = kmp(text, pattern);
-
 	for (auto it = result.begin(); it != result.end(); ++it)
 	{
 		std::copy(*it, *it + pattern.size(), _oit);
 		cout << endl;
 	}
 
+
+	cout << "\n\n" << endl;
+
+
+	auto result2 = kmp_template(text.begin(), text.end(), pattern.begin(), pattern.end());
+	for (auto it = result2.begin(); it != result2.end(); ++it)
+	{
+		std::copy(*it, *it + pattern.size(), _oit);
+		cout << endl;
+	}
 
 
 	system("pause");
